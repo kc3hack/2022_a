8bd88d70a9fa969d576ff50abb7f133db58ad56a
RosesAndViolets <61640805+RosesAndViolets@users.noreply.github.com>
2022-03-27 03:13:22 +0900

    After Firebase

--- PlateLickApp/lib/main.dart
+++ PlateLickApp/lib/main.dart
@@ -1,13 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_todo/pages/Drawerhiden/hiddendrawer.dart';
+import 'package:flutterfire_ui/auth.dart';
 import 'package:provider/provider.dart';
 import 'data/shared/Task_saved.dart';
 import 'data/thems.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await TaskerPreference.init(); // for initial SharedPerfomance ..
+  await Firebase.initializeApp(
+    //for initial firebase
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -26,7 +36,26 @@ class MyApp extends StatelessWidget {
           themeMode: themProvider.themeMode,
           darkTheme: Mytheme.darkthem,
           theme: Mytheme.lightthem,
-          home: HiddenDrawer(),
+          home: AuthGate(),
         );
       });
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return SignInScreen(providerConfigs: []);
+        }
+
+        // Render your application if authenticated
+        return HiddenDrawer();
+      },
+    );
+  }
 }
